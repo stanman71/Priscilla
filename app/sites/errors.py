10cffd0d4d53a0ae4bfe213cc25d957b4f1435ff
stanman71 <stanlay@gmx.net>
2019-09-26 12:00:24 +0200

    add errors page

--- app/sites/errors.py
+++ app/sites/errors.py
@@ -1,3 +1,5 @@
+from flask import json, url_for, redirect, render_template, flash, g, session, jsonify, request
+
 from app import app
 
 
@@ -10,7 +12,6 @@ def http_err(err_code):
 
     elif 404 == err_code:    
         err_msg  = "The URL you were looking for does not seem to exist."
-        err_msg += "<br /> Define the new page in /pages"
     
     elif 500 == err_code:    
         err_msg = "Internal error. Contact the manager about this."
@@ -21,23 +22,109 @@ def http_err(err_code):
     return err_msg
     
 
+@app.errorhandler(400)
+def e400(e):
+
+    # custommize your page title / description here
+    page_title = 'Login - Flask Dark Dashboard | AppSeed App Generator'
+    page_description = 'Open-Source Flask Dark Dashboard, login page.'
+
+    data          = {'navigation': 'None', 'notification': 'None'}
+    error_message = http_err( 400)
+
+    return render_template( 'layouts/default.html',
+                            data=data,
+                            title=page_title,
+                            content=render_template( 'pages/errors.html', 
+                                                     error_message=error_message) 
+                            )
+
+
 @app.errorhandler(401)
 def e401(e):
-    return http_err( 401) # "It seems like you are not allowed to access this link."
 
-@app.errorhandler(404)
-def e404(e):
-    return http_err( 404) # "The URL you were looking for does not seem to exist.<br><br>
-	                      # If you have typed the link manually, make sure you've spelled the link right."
+    # custommize your page title / description here
+    page_title = 'Login - Flask Dark Dashboard | AppSeed App Generator'
+    page_description = 'Open-Source Flask Dark Dashboard, login page.'
 
-@app.errorhandler(500)
-def e500(e):
-    return http_err( 500) # "Internal error. Contact the manager about this."
+    data          = {'navigation': 'None', 'notification': 'None'}
+    error_message = http_err( 401)
+
+    return render_template( 'layouts/default.html',
+                            data=data,
+                            title=page_title,
+                            content=render_template( 'pages/errors.html', 
+                                                     error_message=error_message) 
+                            )
+
 
 @app.errorhandler(403)
 def e403(e):
-    return http_err( 403 ) # "Forbidden access."
+
+    # custommize your page title / description here
+    page_title = 'Login - Flask Dark Dashboard | AppSeed App Generator'
+    page_description = 'Open-Source Flask Dark Dashboard, login page.'
+
+    data          = {'navigation': 'None', 'notification': 'None'}
+    error_message = http_err( 403)
+
+    return render_template( 'layouts/default.html',
+                            data=data,
+                            title=page_title,
+                            content=render_template( 'pages/errors.html', 
+                                                     error_message=error_message) 
+                            )
+
+
+@app.errorhandler(404)
+def e404(e):
+
+    # custommize your page title / description here
+    page_title = 'Login - Flask Dark Dashboard | AppSeed App Generator'
+    page_description = 'Open-Source Flask Dark Dashboard, login page.'
+
+    data          = {'navigation': 'None', 'notification': 'None'}
+    error_message = http_err( 404)
+
+    return render_template( 'layouts/default.html',
+                            data=data,
+                            title=page_title,
+                            content=render_template( 'pages/errors.html', 
+                                                     error_message=error_message) 
+                            )
+
 
 @app.errorhandler(410)
 def e410(e):
-    return http_err( 410) # "The content you were looking for has been deleted."
+
+    # custommize your page title / description here
+    page_title = 'Login - Flask Dark Dashboard | AppSeed App Generator'
+    page_description = 'Open-Source Flask Dark Dashboard, login page.'
+
+    data          = {'navigation': 'None', 'notification': 'None'}
+    error_message = http_err( 410)
+
+    return render_template( 'layouts/default.html',
+                            data=data,
+                            title=page_title,
+                            content=render_template( 'pages/errors.html', 
+                                                     error_message=error_message) 
+                            )
+
+
+@app.errorhandler(500)
+def e500(e):
+
+    # custommize your page title / description here
+    page_title = 'Login - Flask Dark Dashboard | AppSeed App Generator'
+    page_description = 'Open-Source Flask Dark Dashboard, login page.'
+
+    data          = {'navigation': 'None', 'notification': 'None'}
+    error_message = http_err( 500)
+
+    return render_template( 'layouts/default.html',
+                            data=data,
+                            title=page_title,
+                            content=render_template( 'pages/errors.html', 
+                                                     error_message=error_message) 
+                            )
